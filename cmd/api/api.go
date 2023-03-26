bea235b4ebf03b7a4057b44f413749f315d72892
AJackTi <dtrong97vn@gmail.com>
2023-03-26 18:40:59 +0700

    feat: update api backend and handle func error card

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -52,9 +52,8 @@ func (app *application) serve() error {
 func main() {
 	var cfg config
 
-	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
-	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production}")
-	flag.StringVar(&cfg.api, "api", "http://localhost:4001", "URL to api")
+	flag.IntVar(&cfg.port, "port", 4001, "Server port to listen on")
+	flag.StringVar(&cfg.env, "env", "development", "Application environment {development|production|maintenance}")
 
 	flag.Parse()
 
@@ -64,19 +63,13 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	tc := make(map[string]*template.Template)
-
 	app := &application{
-		config:        cfg,
-		infoLog:       infoLog,
-		errorLog:      errorLog,
-		templateCache: tc,
-		version:       version,
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
 	}
 
-	err := app.serve()
-	if err != nil {
-		app.errorLog.Println(err)
+	if err := app.serve(); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/AJackTi/go-stripe
 
 go 1.20
 
-require github.com/go-chi/chi/v5 v5.0.8
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/cors v1.2.1
+)
 
-require github.com/stripe/stripe-go/v74 v74.13.0 // indirect
+require github.com/stripe/stripe-go/v74 v74.13.0
